ce279829d6a1ac9d65b56be7cfc2484c09f8f4a4
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-08 14:10:31 +0530

    Django Partial Setup

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,8 @@ from frontend.views import home, qr_code_view, webcam_feed, register
 
 urlpatterns = [
     path('', home, name='home'),
+    path('/', home, name='home'),
+    path('/home', home, name='home'),
     path('qr/', qr_code_view, name='qr-code'),
     path('webcam/', webcam_feed, name='webcam-feed'),
     path('register', register, name='register')
