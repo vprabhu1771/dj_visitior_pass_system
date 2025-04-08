83a1d1c253a7e661ece220037df569a66a92dee6
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-08 12:21:34 +0530

    home logic added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,8 @@
 # urls.py
 from django.urls import path
-from . import views
+from frontend.views import home, qr_code_view
 
 urlpatterns = [
-    path('qr/', views.qr_code_view, name='qr-code'),
+    path('', home, name='home'),
+    path('qr/', qr_code_view, name='qr-code'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,20 +1,29 @@
-# views.py
 from django.http import HttpResponse, Http404
+from django.shortcuts import render
 from .utils import generate_qr_code
 from backend.models import CustomUser
 
 
+def home(request):
+    user = None
+    query = request.GET.get('phone_no')
+    if query:
+        try:
+            user = CustomUser.objects.get(phone_no=query)
+        except CustomUser.DoesNotExist:
+            user = None
+    return render(request, "frontend/home.html", {"user": user})
+
+
 def qr_code_view(request):
-    user_id = request.GET.get('id')  # expects ?id=1
+    user_id = request.GET.get('id')
     if not user_id:
         return HttpResponse("User ID not provided", status=400)
-
     try:
         user = CustomUser.objects.get(id=user_id)
     except CustomUser.DoesNotExist:
         raise Http404("User not found")
 
-    # Customize QR data as needed
     data = f"ID: {user.id}\nName: {user.first_name} {user.last_name}\nEmail: {user.email}\nPhone: {user.phone_no}"
 
     qr_image = generate_qr_code(data)
